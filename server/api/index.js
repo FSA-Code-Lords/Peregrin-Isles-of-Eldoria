18cf8650d64a1711d52678c24eff968409eaa7b2
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-09-11 15:01:19 -0400

    users page CRUD created

--- server/api/index.js
+++ server/api/index.js
@@ -10,5 +10,6 @@ router.use(`/classes`, require(`./classes`));
 router.use(`/races`, require(`./races`));
 router.use(`/items`, require(`./items`));
 router.use(`/monsters`, require(`./monsters`));
+router.use(`/users`, require(`./users`));
 
 module.exports = router;

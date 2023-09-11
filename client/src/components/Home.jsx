2ed526e136bb4c7d7fac90964f16801ce156b6f3
Eddie Jacobian <edjacobian@gmail.com>
2023-09-11 11:46:01 -0400

    moved out navbar and nav buttons into home comp

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,44 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleNewGameClick = () => {
+    navigate('/newgame');
+  };
+
+  const handleCreditsClick = () => {
+    navigate('/credits');
+  }
+
+  const handleAboutClick = () => {
+    navigate('/about');
+  }
+
+  const handleLoadGameClick = () => {
+    navigate('/loadgame');
+  };
+
   return (
     <>
-      <figure class="mainlogo-container">
-        <h1 class="logo-text">Peregin Isles of Eldoria</h1>
+      <figure className="mainlogo-container">
+        <h1 className="logo-text">Peregrin Isles of Eldoria</h1>
       </figure>
+      <div id="button-container">
+        <button className="nav-button" onClick={handleNewGameClick}>
+          New Game
+        </button>
+        <button className="nav-button" onClick={handleLoadGameClick}>
+          Load Game
+        </button>
+        <button className="nav-button" onClick={handleAboutClick}>
+          About Game
+        </button>
+        <button className="nav-button" onClick={handleCreditsClick}>
+          Credits
+        </button>
+      </div>
     </>
   );
 };
--- client/src/components/bars/Navbar.jsx
+++ client/src/components/bars/Navbar.jsx
@@ -1,61 +1,9 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import LoadGame from '../game/LoadGame';
-import NewGame from '../game/NewGame';
-import About from '../About';
-import Credits from '../Credits';
-
-const ButtonNavigation = () => {
-  const navigate = useNavigate();
-
-  const handleNewGameClick = () => {
-    navigate('/newgame');
-  };
-
-  const handleCreditsClick = () => {
-    navigate('/credits');
-  }
-
-  const handleAboutClick = () => {
-    navigate('/about');
-  }
-
-  const handleLoadGameClick = () => {
-    navigate('/loadgame');
-  };
-
-  return (
-    <div id="button-container">
-      <button className="nav-button" onClick={handleNewGameClick}>
-        New Game
-      </button>
-      <button className="nav-button" onClick={handleLoadGameClick}>
-        Load Game
-      </button>
-      <button className="nav-button" onClick={handleAboutClick}>
-        About Game
-      </button>
-      <button className="nav-button" onClick={handleCreditsClick}>
-        Credits
-      </button>
-    </div>
-  );
-};
-
-const CustomNavbar = () => {
+const Navbar = () => {
   return (
     <>
-      <ButtonNavigation />
-      <div>
-        <Routes>
-          <Route path="/newgame" element={<NewGame />} />
-          <Route path="/loadgame" element={<LoadGame />} />
-          <Route path="/credits" element={<Credits />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+
     </>
   );
 };
 
-export default CustomNavbar;
+export default Navbar;

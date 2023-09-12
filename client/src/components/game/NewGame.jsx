61010fbca312541c540736377a115c4bc9e658ab
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-09-12 17:05:45 -0400

    new game page probably done. it is working

--- client/src/components/game/NewGame.jsx
+++ client/src/components/game/NewGame.jsx
@@ -1,38 +1,101 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NewGame = () => {
-  const [newCharacterData, setNewCharacterData] = useState({
-    name: "",
-    race: "",
-    class: "",
-    hp: 10,
-    atk: 5,
-    dodge: 5,
+  const navigate = useNavigate();
+  const baseStats = { hp: 20, atk: 10, dodge: 10 };
+  const [characterName, setCharacterName] = useState(``);
+  const [characterClass, setCharacterClass] = useState({
+    hpChange: 0,
+    atkChange: 0,
+    dodgeChange: 0,
   });
-  const [chosenRaceData, setChosenRaceData] = useState({})
-  const [chosenClassData, setChosenClassData] = useState({})
-
-
-  const [newGameData, setNewGameData] = useState({
-    character: newCharacterData,
+  const [characterRace, setCharacterRace] = useState({
+    hpChange: 0,
+    atkChange: 0,
+    dodgeChange: 0,
   });
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setNewCharacterData({ ...newCharacterData, [name]: value });
+  const newGame = {
+    character: {
+      name: characterName,
+      race: characterRace.name,
+      class: characterClass.name,
+      hp: baseStats.hp + characterClass.hpChange + characterRace.hpChange,
+      atk: baseStats.atk + characterClass.atkChange + characterRace.atkChange,
+      dodge:
+        baseStats.dodge +
+        characterClass.dodgeChange +
+        characterRace.dodgeChange,
+    },
+    quests: [],
+    map: [],
+    inventory: [],
   };
+
+  useEffect(() => {
+    fetchFirstLocation();
+  }, [characterClass, characterRace, characterName]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetchRace(newCharacterData.race);
+    localStorage.setItem(`gameData`, JSON.stringify(newGame));
     event.target.reset();
+    setCharacterClass({
+      hpChange: 0,
+      atkChange: 0,
+      dodgeChange: 0,
+    });
+    setCharacterRace({
+      hpChange: 0,
+      atkChange: 0,
+      dodgeChange: 0,
+    });
+    navigate(`/loadGame`);
   };
 
   const fetchRace = async (id) => {
+    if (id === "Choose Race") {
+      setCharacterRace({
+        hpChange: 0,
+        atkChange: 0,
+        dodgeChange: 0,
+      });
+    } else {
+      try {
+        const response = await fetch(`/api/races/${id}`);
+        const result = await response.json();
+        setCharacterRace(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const fetchClass = async (id) => {
+    if (id === "Choose Class") {
+      setCharacterClass({
+        hpChange: 0,
+        atkChange: 0,
+        dodgeChange: 0,
+      });
+    } else {
+      try {
+        const response = await fetch(`/api/classes/${id}`);
+        const result = await response.json();
+        setCharacterClass(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const fetchFirstLocation = async () => {
     try {
-      const response = await fetch(`/api/races/${id}`);
+      const response = await fetch(`/api/locations/1`);
       const result = await response.json();
-      console.log(result);
+      newGame.map.push(result);
+      newGame.quests = result.quests;
     } catch (error) {
       console.log(error);
     }
@@ -47,13 +110,16 @@ const NewGame = () => {
           <input
             type="text"
             name="name"
-            onChange={handleInputChange}
+            onChange={(event) => setCharacterName(event.target.value)}
             required
           />
         </div>
         <div>
           <label>Race:</label>
-          <select name="race" onChange={handleInputChange}>
+          <select
+            name="race"
+            onChange={(event) => fetchRace(event.target.value)}
+          >
             <option>Choose Race</option>
             <option value="1">Human</option>
             <option value="2">Elf</option>
@@ -62,7 +128,10 @@ const NewGame = () => {
         </div>
         <div>
           <label>Class:</label>
-          <select name="class" onChange={handleInputChange}>
+          <select
+            name="class"
+            onChange={(event) => fetchClass(event.target.value)}
+          >
             <option>Choose Class</option>
             <option value="1">Warrior</option>
             <option value="2">Assassin</option>
@@ -72,6 +141,24 @@ const NewGame = () => {
         </div>
         <button type="submit">Create Character</button>
       </form>
+      {characterRace.name ? (
+        <section>
+          <p>Name: {characterRace.name}</p>
+          <p>Description: {characterRace.description}</p>
+          <p>HP Change: {characterRace.hpChange}</p>
+          <p>ATK Change: {characterRace.atkChange}</p>
+          <p>Dodge Change: {characterRace.dodgeChange}</p>
+        </section>
+      ) : null}
+      {characterClass.name ? (
+        <section>
+          <p>Name: {characterClass.name}</p>
+          <p>Description: {characterClass.description}</p>
+          <p>HP Change: {characterClass.hpChange}</p>
+          <p>ATK Change: {characterClass.atkChange}</p>
+          <p>Dodge Change: {characterClass.dodgeChange}</p>
+        </section>
+      ) : null}
     </div>
   );
 };

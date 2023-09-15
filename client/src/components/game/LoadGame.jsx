92380c11eb300d5ce373678ef18c57480c0c158e
Eddie Jacobian <edjacobian@gmail.com>
2023-09-15 14:25:38 -0400

    added load game main menu button and styling

--- client/src/components/game/LoadGame.jsx
+++ client/src/components/game/LoadGame.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const LoadGame = () => {
   const [saveData, setsaveData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch('/api/saveData')
@@ -18,24 +21,31 @@ const LoadGame = () => {
       });
   }, []);
 
+  const handleMainMenuClick = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Load Game</h1>
-      {isLoading ? (
-        <p>Loading saved games...</p>
-      ) : (
-        <ul>
-          {saveData.map((saveData) => (
-            <li key={saveData.id}>
-              {/* Render saved game details here */}
-              <p>Name: {saveData.name}</p>
-              <p>Race: {saveData.race}</p>
-              <p>Class: {saveData.class}</p>
-              {/* Add more details as needed */}
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="formstyle">
+      <div className="form-container">
+        <h1>Load Game</h1>
+        {isLoading ? (
+          <p>Loading saved games...</p>
+        ) : (
+          <ul>
+            {saveData.map((saveData) => (
+              <li key={saveData.id}>
+                {/* Render saved game details here */}
+                <p>Name: {saveData.name}</p>
+                <p>Race: {saveData.race}</p>
+                <p>Class: {saveData.class}</p>
+                {/* Add more details as needed */}
+              </li>
+            ))}
+          </ul>
+        )}
+        <button onClick={handleMainMenuClick}>Return To Main Menu</button>
+      </div>
     </div>
   );
 };
